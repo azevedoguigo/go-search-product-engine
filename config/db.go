333b5366cf40460084d11b55d4cd39e86ee74de9
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-12-03 20:03:15 -0300

    chore: database configuration.

---
+++ config/db.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+type DBConfig struct {
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+}
+
+func LoadDatabaseConfig() *DBConfig {
+	return &DBConfig{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		DBPort:     os.Getenv("DB_PORT"),
+	}
+}
+
+func InitDB() *gorm.DB {
+	cfg := LoadDatabaseConfig()
+
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect database:", err.Error())
+	}
+
+	return db
+}
