8a74a9e082d234014e227baec64ac04a827424a7
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-12-04 16:14:44 -0300

    refactor: project reorganization.

--- internal/elasticsearch.go
+++ internal/elasticsearch.go
@@ -5,26 +5,8 @@ import (
 	"encoding/json"
 	"log"
 
-	"github.com/google/uuid"
 	"github.com/olivere/elastic/v7"
-	"gorm.io/gorm"
 )
-
-type Product struct {
-	ID          uuid.UUID `json:"id"`
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Price       *float64  `json:"price,omitempty"`
-	CategoryID  uuid.UUID `json:"category_id"`
-}
-
-func FetchProductsFromDB(db *gorm.DB) ([]Product, error) {
-	var products []Product
-	if err := db.Find(&products).Error; err != nil {
-		return nil, err
-	}
-	return products, nil
-}
 
 func IndexProducts(client *elastic.Client, products []Product) {
 	for _, product := range products {
